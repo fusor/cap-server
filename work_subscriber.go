8f61e4e7cd03637fe8ea6e5170539e4e1425ce1b
Erik Nelson <erik@nsk.io>
2016-11-22 14:53:29 -0500

    Initial addition of workengine structs

---
+++ work_subscriber.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+type IWorkSubscriber interface {
+	Subscribe(msgBuffer <-chan string)
+}
+
+////////////////////////////////////////////////////////////
+// Example stdout subscriber to illustrate how decoupled the
+// subscribers actually are from the work engine, and how
+// they can perform arbirary processing with the work messages
+// as long as the IWorkSubscriber interface is implemented
+////////////////////////////////////////////////////////////
+func SubscriberFactory(sub string) IWorkSubscriber {
+	// Totally unncessary, but cool nonetheless
+	if sub == "socket" {
+		return NewSocketWorkSubscriber()
+	} else {
+		return &StdoutWorkSubscriber{}
+	}
+}
+
+// Example Subscriber
+type StdoutWorkSubscriber struct {
+	msgBuffer <-chan string
+}
+
+func (s *StdoutWorkSubscriber) Subscribe(msgBuffer <-chan string) {
+	// Always drain the buffer if there's a message waiting.
+	// Here we're just forwarding to stdout, but of course, the message
+	// destination could be anything (ultimate websockets!)
+	// NOTE: DON'T FORGET TO GOROUTINE THIS, OR WILL YOU CHOKE THE MAIN PROCESSOR
+	s.msgBuffer = msgBuffer
+	go func() {
+		for {
+			msg := <-msgBuffer
+			fmt.Printf(msg)
+		}
+	}()
+}
