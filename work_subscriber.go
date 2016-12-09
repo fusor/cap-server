68ca0306a3cfea000b8c2b1de867d5ace527e755
Erik Nelson <erik@nsk.io>
2016-12-09 11:35:26 -0500

    Review update: drop "I" prefix from interfaces

--- work_subscriber.go
+++ work_subscriber.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-type IWorkSubscriber interface {
-	Subscribe(msgBuffer <-chan IWorkMsg)
+type WorkSubscriber interface {
+	Subscribe(msgBuffer <-chan WorkMsg)
 }
 
-type IWorkMsg interface {
+type WorkMsg interface {
 	Render() string
 }
 
@@ -16,9 +16,9 @@ type IWorkMsg interface {
 // Example stdout subscriber to illustrate how decoupled the
 // subscribers actually are from the work engine, and how
 // they can perform arbirary processing with the work messages
-// as long as the IWorkSubscriber interface is implemented
+// as long as the WorkSubscriber interface is implemented
 ////////////////////////////////////////////////////////////
-func SubscriberFactory(sub string) IWorkSubscriber {
+func SubscriberFactory(sub string) WorkSubscriber {
 	// Totally unncessary, but cool nonetheless
 	if sub == "socket" {
 		return NewSocketWorkSubscriber()
@@ -29,10 +29,10 @@ func SubscriberFactory(sub string) IWorkSubscriber {
 
 // Example Subscriber
 type StdoutWorkSubscriber struct {
-	msgBuffer <-chan IWorkMsg
+	msgBuffer <-chan WorkMsg
 }
 
-func (s *StdoutWorkSubscriber) Subscribe(msgBuffer <-chan IWorkMsg) {
+func (s *StdoutWorkSubscriber) Subscribe(msgBuffer <-chan WorkMsg) {
 	// Always drain the buffer if there's a message waiting.
 	// Here we're just forwarding to stdout, but of course, the message
 	// destination could be anything (ultimate websockets!)
